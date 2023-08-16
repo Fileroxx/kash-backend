e7e8515cae4d7cbd67aba8752fb71fcdd44fa550
Igor <igor_cesari@hotmail.com>
2023-08-16 17:46:37 -0300

    login também

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const allowedOrigins = [
   'http://127.0.0.1:5173',
   'https://smartfinsoluction-backend.vercel.app',
   'https://smartfin-soluction.vercel.app',
-  'https://smartfin-soluction-git-feature-home-fileroxx.vercel.app/'
+  'https://smartfin-soluction-git-feature-home-fileroxx.vercel.app/',
+  'https://smartfin-soluction-git-feature-home-fileroxx.vercel.app/login'
 ];
 
 // Aplicar as opções de configuração do CORS
